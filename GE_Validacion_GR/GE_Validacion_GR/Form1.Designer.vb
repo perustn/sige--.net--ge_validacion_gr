e489ea05442fa5280d1b7ca25e0c8b0a8f8fcf2a
Miguel Manuel Leon Noa <mleon@perustn.com>
2024-06-18 15:41:59 -0500

    FEAT: NUEVO PROYECTO

---
+++ GE_Validacion_GR/GE_Validacion_GR/Form1.Designer.vb
@@ -0,0 +1,138 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.panHeader = New System.Windows.Forms.Panel()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.lblOrigen = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.tmProgramacion = New System.Windows.Forms.Timer(Me.components)
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.panHeader.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'panHeader
+        '
+        Me.panHeader.BackColor = System.Drawing.Color.Transparent
+        Me.panHeader.Controls.Add(Me.btnSalir)
+        Me.panHeader.Controls.Add(Me.lblOrigen)
+        Me.panHeader.Controls.Add(Me.PictureBox1)
+        Me.panHeader.Controls.Add(Me.PictureBox2)
+        Me.panHeader.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.panHeader.Location = New System.Drawing.Point(0, 0)
+        Me.panHeader.Margin = New System.Windows.Forms.Padding(4)
+        Me.panHeader.Name = "panHeader"
+        Me.panHeader.Size = New System.Drawing.Size(542, 118)
+        Me.panHeader.TabIndex = 206
+        '
+        'btnSalir
+        '
+        Me.btnSalir.BackColor = System.Drawing.Color.AliceBlue
+        Me.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnSalir.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSalir.ForeColor = System.Drawing.Color.SteelBlue
+        Me.btnSalir.Location = New System.Drawing.Point(96, 53)
+        Me.btnSalir.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(352, 28)
+        Me.btnSalir.TabIndex = 10
+        Me.btnSalir.Text = "Cerrar proceso de verificación"
+        Me.btnSalir.UseVisualStyleBackColor = False
+        '
+        'lblOrigen
+        '
+        Me.lblOrigen.AutoSize = True
+        Me.lblOrigen.BackColor = System.Drawing.Color.Transparent
+        Me.lblOrigen.Cursor = System.Windows.Forms.Cursors.Default
+        Me.lblOrigen.Font = New System.Drawing.Font("Calibri", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblOrigen.ForeColor = System.Drawing.Color.Gray
+        Me.lblOrigen.Location = New System.Drawing.Point(91, 23)
+        Me.lblOrigen.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblOrigen.Name = "lblOrigen"
+        Me.lblOrigen.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.lblOrigen.Size = New System.Drawing.Size(415, 29)
+        Me.lblOrigen.TabIndex = 8
+        Me.lblOrigen.Text = "Revisando Guías de Remisión por Enviar"
+        Me.lblOrigen.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(17, 17)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(32, 30)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 7
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.GE_Validacion_GR.My.Resources.Resources.SigeMail1
+        Me.PictureBox2.Location = New System.Drawing.Point(20, 11)
+        Me.PictureBox2.Margin = New System.Windows.Forms.Padding(4)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(67, 80)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 9
+        Me.PictureBox2.TabStop = False
+        '
+        'tmProgramacion
+        '
+        Me.tmProgramacion.Interval = 5000
+        '
+        'BackgroundWorker1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(542, 118)
+        Me.Controls.Add(Me.panHeader)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "SIGE GUÍAS DE REMISION CONFECCIONES (EXPORTACIÓN)"
+        Me.panHeader.ResumeLayout(False)
+        Me.panHeader.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents panHeader As Panel
+    Friend WithEvents btnSalir As Button
+    Public WithEvents lblOrigen As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents tmProgramacion As Timer
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+End Class
---
+++ GE_Validacion_GR/GE_Validacion_GR/Form1.vb
@@ -0,0 +1,229 @@
+﻿Imports System.ComponentModel
+Imports System.Configuration
+Imports System.Collections.Generic
+Imports System.Threading
+Imports System.IO
+Imports System.Net
+Imports System.Text
+Imports Newtonsoft.Json
+Public Class Form1
+    Private colEmpresa As Color
+    Property CodEmpresa As String
+    Private oHP As clsHELPER = New clsHELPER
+    Private strSQL As String
+    Private oDt As New DataTable
+    Dim Hilo As Threading.Thread
+    Property CadenaDeConexion_Operaciones_VBNet As String
+    Property CadenaDeConexion_Seguridad_VBNet As String
+    Property CadenaDeConexion_Operaciones_VB6 As String
+    Property CadenaDeConexion_Seguridad_VB6 As String
+    Private Sub DefinirConexionSIGE(strRutaArchivoSIGEConfig As String, Optional ByVal strBD As String = "SIGE_STN")
+        Dim oSIGELink As New SIGELink.clsSIGELink
+
+        If My.Computer.FileSystem.FileExists(strRutaArchivoSIGEConfig) = False Then
+            MessageBox.Show("No existe el archivo de conexión SIGELink, deberia encontrarse en la siguiente ruta " & vbNewLine & strRutaArchivoSIGEConfig,
+                            "SIGE : Autenticación",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information)
+            Return
+        End If
+
+        If oSIGELink.LeerArchivoDeConexionSQL(strRutaArchivoSIGEConfig) = False Then
+            Dim strMsgErr As String
+            strMsgErr = "No se ha podido enlazar con el Servidor de Base de Datos, vuelva a generar el archivo SIGELink"
+            MessageBox.Show(strMsgErr,
+                            "SIGE : Autenticación",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information)
+            Application.Exit()
+            Exit Sub
+        End If
+
+        Select Case oSIGELink.TipoDeConexion
+            Case SIGELink.clsSIGELink.enuTipoDeConexion.MonoEmpresa
+                CadenaDeConexion_Operaciones_VBNet = oSIGELink.DevuelveCadenaDeConexionConBDSIGE
+                CadenaDeConexion_Seguridad_VBNet = oSIGELink.DevuelveCadenaDeConexionConBDSeguridad
+
+                CadenaDeConexion_Operaciones_VB6 = oSIGELink.DevuelveCadenaDeConexionConBDSIGE_EnVB6
+                CadenaDeConexion_Seguridad_VB6 = oSIGELink.DevuelveCadenaDeConexionConBDSeguridad_EnVB6
+
+            Case SIGELink.clsSIGELink.enuTipoDeConexion.MultiEmpresa
+                CadenaDeConexion_Operaciones_VBNet = oSIGELink.DevuelveCadenaDeConexionConBDSIGE & strBD
+                CadenaDeConexion_Seguridad_VBNet = oSIGELink.DevuelveCadenaDeConexionConBDSeguridad
+
+                CadenaDeConexion_Operaciones_VB6 = oSIGELink.DevuelveCadenaDeConexionConBDSIGE_EnVB6 & strBD
+                CadenaDeConexion_Seguridad_VB6 = oSIGELink.DevuelveCadenaDeConexionConBDSeguridad_EnVB6
+        End Select
+
+
+    End Sub
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub RevisandoProcesosPorNotificar()
+        Try
+            Dim sFecha As String
+            Dim sHora As String
+            Dim Cad_Traza As String = ""
+            Dim Dt_URL As New DataTable
+            Dim url As String
+            Dim docid As String
+            Dim comando As String = "emitir"
+            Dim parametro As String = ""
+            'Dim Rsp_Mail As String
+
+            sFecha = Date.Now.Year.ToString + Microsoft.VisualBasic.Right("00" + Date.Now.Month.ToString, 2) + Microsoft.VisualBasic.Right("00" + Date.Now.Day.ToString, 2)
+            sHora = Now.ToString("HH:mm")
+            'strSQL = String.Format("EXEC SIGE_Notificador '{0}'´,'{1}','{2}'", "W", sFecha, sHora)
+
+            strSQL = String.Empty
+            strSQL &= vbNewLine & "EXEC SIGE_Revisa_GR_Pendiente_Envio"
+            strSQL &= vbNewLine & String.Format(" @Opcion     = '{0}'", "L")
+            strSQL &= vbNewLine & String.Format(",@Fecha      = '{0}'", sFecha)
+            strSQL &= vbNewLine & String.Format(",@Hora       = '{0}'", sHora)
+
+            oDt = oHP.DevuelveDatos(strSQL, Me.CadenaDeConexion_Operaciones_VBNet)
+            If oDt Is Nothing OrElse oDt.Rows.Count = 0 Then Return
+
+            Dt_URL = oHP.DevuelveDatos("SP_IP_SERVER_ACEPTA_GR", CadenaDeConexion_Operaciones_VBNet)
+            If Dt_URL.Rows.Count > 0 Then
+                url = Dt_URL.Rows(0)(0).ToString
+            Else
+                MessageBox.Show("No se Encontro Direccion IP del Servidor", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                Exit Sub
+            End If
+
+            For i = 0 To oDt.Rows.Count - 1
+
+                Cad_Traza = oDt.Rows(i)(5).ToString()
+
+                'url = "http://192.168.30.9:5001/ca4xml"
+                '-------------------------------------------------
+                docid = "T" + oDt.Rows(i)(3).ToString() & "-" & oDt.Rows(i)(4).ToString()
+                'Dim comando As String = "emitir"
+                'Dim parametro As String = ""
+                Dim respuesta As String = SendCA4XMLRequest(url, docid, comando, parametro, Cad_Traza)
+
+                strSQL = String.Format("EXEC Ventas_Distribuye_por_Partida_Actualzia_Envio_GR_CMT_Rsp_Acepta '{0}','{1}','{2}','{3}','{4}','{5}','{6}'", oDt.Rows(i)(0), oDt.Rows(i)(1).ToString(), oDt.Rows(i)(2).ToString(), oDt.Rows(i)(3).ToString(), oDt.Rows(i)(4).ToString(), respuesta, "AUTO")
+                If (respuesta.Substring(0, 2).ToUpper = "OK") Then
+                    If oHP.EjecutaOperacion(strSQL, CadenaDeConexion_Operaciones_VBNet) Then
+                    End If
+                Else
+                    If oHP.EjecutaOperacion(strSQL, CadenaDeConexion_Operaciones_VBNet) Then
+                    End If
+                End If
+
+            Next
+
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Public Function SendCA4XMLRequest(ByVal url As String, ByVal docid As String, ByVal comando As String, ByVal parametros As String, ByVal Datos As String) As String
+        Dim result As String = ""
+        Dim Solicitud As HttpWebRequest
+        Dim Respuesta As HttpWebResponse
+        Dim cuerpo_peticion As String = ""
+        cuerpo_peticion = ("docid=" _
+                    + (EncodeURL(docid) + ("&comando=" _
+                    + (EncodeURL(comando) + ("&parametros=" _
+                    + (EncodeURL(parametros) + ("&datos=" + EncodeURL(Datos))))))))
+        Solicitud = CType(WebRequest.Create(url), HttpWebRequest)
+        Try
+            Dim bytes() As Byte
+            bytes = System.Text.Encoding.UTF8.GetBytes(cuerpo_peticion)
+            'Set HttpWebRequest properties
+            Solicitud.Method = "POST"
+            Solicitud.ContentLength = bytes.Length
+            Solicitud.ContentType = "application/x-www-form-urlencoded"
+            'Solicitud.Connection = "Close";
+            Dim requestStream As Stream = Solicitud.GetRequestStream
+            'Writes a sequence of bytes to the current stream 
+            requestStream.Write(bytes, 0, bytes.Length)
+            requestStream.Close()
+            'Close stream                
+            Respuesta = CType(Solicitud.GetResponse, HttpWebResponse)
+            If (Respuesta.StatusCode = HttpStatusCode.OK) Then
+                'Get response stream into StreamReader
+                Dim responseStream As Stream = Respuesta.GetResponseStream
+                Dim reader As StreamReader = New StreamReader(responseStream)
+                result = reader.ReadToEnd
+            Else
+                result = ("ERROR|||||||Error del servidor: " _
+                            + (Respuesta.StatusCode + (": " + Respuesta.StatusDescription)))
+            End If
+
+            Respuesta.Close()
+            'Close HttpWebResponse
+        Catch we As WebException
+            result = ("ERROR|||||||Error del servidor: " _
+                        + (we.Status + (": " + we.Message)))
+        Catch ex As Exception
+            result = ("ERROR|||||||Error del servidor: " + ex.Message)
+            Throw New Exception(ex.Message)
+        Finally
+            'Respuesta.Close();
+            'Release objects
+            Respuesta = Nothing
+            Solicitud = Nothing
+        End Try
+
+        Return result
+    End Function
+
+    Private Function EncodeURL(data As String) As String
+        Dim result As String = ""
+        Dim aChar As String
+        Dim theCode As Integer
+        Dim N As Integer
+        Dim aux As String
+        For N = 0 To data.Length - 1
+            aChar = data.Substring(N, 1)
+            theCode = Microsoft.VisualBasic.AscW(aChar)
+            If (theCode >= 0 AndAlso theCode <= 47) OrElse (theCode >= 58 AndAlso theCode <= 64) OrElse (theCode >= 91 AndAlso theCode <= 93) OrElse (theCode >= 123 AndAlso theCode <= 126) Then
+                aux = (theCode + 256).ToString("X")
+                result = result & "%" & aux.Substring(aux.Length - 2, 2)
+
+            Else
+                result = result + aChar
+
+            End If
+
+        Next
+        Return result
+
+    End Function
+
+    Private Sub tmProgramacion_Tick(sender As Object, e As EventArgs) Handles tmProgramacion.Tick
+        'Hilo = New Threading.Thread(AddressOf RevisandoProcesosPorNotificar)
+        'Hilo.Start()
+        RevisandoProcesosPorNotificar()
+    End Sub
+
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Me.CodEmpresa = ConfigurationSettings.AppSettings("Empresa").ToString
+        'DefinirConexionSIGE(ConfigurationSettings.AppSettings("SIGELink").ToString, "SEGURIDAD")
+
+        Me.CadenaDeConexion_Operaciones_VBNet = "Initial Catalog=sige_stn;Data Source=192.168.30.55;Integrated Security = SSPI"
+        Me.CadenaDeConexion_Seguridad_VBNet = "Initial Catalog=Seguridad;Data Source=192.168.30.55;Integrated Security = SSPI"
+
+        Dim oDt As DataTable = oHP.DevuelveDatos(String.Format("Select * FROM SEG_Empresas where cod_empresa = '{0}'", Me.CodEmpresa), Me.CadenaDeConexion_Seguridad_VBNet)
+        'colEmpresa = Color.FromArgb(oDt.Rows(0)("ColorFondo_R"), oDt.Rows(0)("ColorFondo_G"), oDt.Rows(0)("ColorFondo_B"))
+        colEmpresa = Color.FromArgb(193, 208, 222)
+
+        tmProgramacion.Interval = 60000 * 30
+        tmProgramacion.Enabled = True
+        'Timer = New Timer();
+        '    Timer.Interval = 1000; // Intervalo de 1 segundo (1000 milisegundos)
+        '    Timer.Enabled = True;
+        '    Timer.Tick += Timer_Tick;
+        'tmProgramacion_Tick(sender, e)
+    End Sub
+End Class
